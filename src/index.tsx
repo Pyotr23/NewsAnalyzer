3870bc0c5c57eef8c77fcde7627015622cc3ab06
Pyotr <2chilavert3@mail.ru>
2020-08-01 13:23:35 +0300

    Вёрстка в целом окончена, начинаются "мелкие" правки.

--- src/index.tsx
+++ src/index.tsx
@@ -13,8 +13,6 @@ ReactDOM.render(
     <BrowserRouter>
       <App>
         <Switch>
-          <Route exact path="/" component={About} />
-          {/* <Route exact path="/" component={Analytics} /> */}
           <Route exact path="/" component={Home} />
           <Route path="/about" component={About} />
           <Route path="/analytics" component={Analytics} />
