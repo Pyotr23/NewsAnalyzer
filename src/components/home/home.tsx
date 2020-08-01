3f5b0987ca8371a6e3dbbc8a8b7e5042a6c58c84
Pyotr <2chilavert3@mail.ru>
2020-08-02 01:45:16 +0300

    Включена вёрстка неудачных результатов.

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import HeaderWrapper from "../header-wrapper/header-wrapper";
 import Cards from "../cards/cards";
 import Author from "../author/author";
+import Loading from "../loading/loading";
+import NoResult from "../no-result/no-result";
 
 const Home = () => (
   <>
     <HeaderWrapper />
+    <Loading />
+    <NoResult />
     <Cards />
     <Author />
   </>
